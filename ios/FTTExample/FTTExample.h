3809f1045e59cda1f5c961e0e69a45c43aba64e4
futianitan <futiantian@ppdai.com>
2018-02-28 11:48:24 +0800

    提交

---
+++ ios/FTTExample/FTTExample.h
@@ -0,0 +1,13 @@
+//
+//  FTTExample.h
+//  FTTExample
+//
+//  Created by futiantian on 2018/2/28.
+//  Copyright © 2018年 futianitan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FTTExample : NSObject
+
+@end
---
+++ ios/FTTExample/FTTExample.m
@@ -0,0 +1,13 @@
+//
+//  FTTExample.m
+//  FTTExample
+//
+//  Created by futiantian on 2018/2/28.
+//  Copyright © 2018年 futianitan. All rights reserved.
+//
+
+#import "FTTExample.h"
+
+@implementation FTTExample
+
+@end
